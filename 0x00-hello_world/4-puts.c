c4e45cec7121872b9055c5c141ee53d4cd3a6a57
Bolt202 <botinani85@gmail.com>
2023-10-18 16:44:56 -0700

    Size is not grandeur, and territory does not make a nation

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - 4-puts.c
+ * Description: Write a C program that prints a specific string.
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
+
