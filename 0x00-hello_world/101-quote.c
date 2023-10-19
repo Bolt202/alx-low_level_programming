09a3522799ba93c620f94f92c637f1c63deae4ff
Bolt202 <botinani85@gmail.com>
2023-10-18 17:54:56 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,16 @@
 #include <unistd.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: prints a string to the standard error without using printf
+ *
+ * Return: Always 1 (Error)
+ */
 int main(void)
 {
-        write(2,
-              "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-              59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
-        return (1);
+	return (1);
 }
+
