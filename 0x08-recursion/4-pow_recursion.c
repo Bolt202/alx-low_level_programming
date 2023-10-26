035e125d310cc3e53625907405f2a1df91df764d
Bolt202 <botinani85@gmail.com>
2023-10-25 17:30:48 -0700

    4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,24 +1,18 @@
 #include "main.h"
+
 /**
-  * _pow_recursion - function name
-  * @x: parameter 1
-  * @y: parameter 2
-  * Return: value of a given number passed to the function
-  */
-
-
+ * _pow_recursion - Computes the power of a number recursively.
+ * @x: The base.
+ * @y: The exponent.
+ *
+ * Return: The result of x^y. If y is negative, returns -1.
+ */
 int _pow_recursion(int x, int y)
 {
-
-if (y < 0)
-        return (-1);
-
-else if (y == 0)
-        return (1);
-
-else
-        return (x * _pow_recursion(x, (y - 1)));
-
-return (0);
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
 }
 
